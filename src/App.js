2f28173012da3515bbdf00cdb9fc1848c1fb8bca
alexfoster1022 <alexander.f.holman@gmail.com>
2022-06-01 17:43:36 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { React, useState } from 'react';
 import './App.css';
 
+import Note from './components/Note';
+
 
 const App = () => {
 
@@ -78,6 +80,22 @@ const renderForm = (
       </div>
     </div>
   )
-}
+} 
+
+
+/* const App = ({ notes }) => {
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+          )}
+      </ul>
+    </div>
+  )
+} */
+
 
 export default App;
